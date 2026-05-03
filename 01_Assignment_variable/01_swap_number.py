4f9251a7da2f1cce79485f3a0dac02d04c98aa2b
Manaskosht <koshtimanas@gmail.com>
2026-05-03 20:02:18 +0530

    swap-num

--- 01_Assignment_variable/01_swap_number.py
+++ 01_Assignment_variable/01_swap_number.py
@@ -1,10 +1,10 @@
-n1=20
-n2=30
+n1 = 20
+n2 = 30
 print(f"Before swap n1 : {n1}")
 print(f"Before swap n2 : {n2}")
 
-x=n1
-n1=n2
-n2=x
+x = n1
+n1 =n2
+n2 = x
 print(f"After swap n1 : {n1}")
 print(f"After swap n2 : {n2}")
---
+++ 01_Assignment_variable/03_swap_int-str.py
@@ -0,0 +1,6 @@
+n1 = 200
+char2 = "java"
+print(f"Before swap n1 : {n1}")
+print(f"Before swap char2 : {char2}")
+
+temp
