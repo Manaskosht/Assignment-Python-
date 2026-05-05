0fea90501664720248ecb4b1286c8ff22a6d71f7
Manaskosht <koshtimanas@gmail.com>
2026-05-05 13:42:53 +0530

    simplified-expression

---
+++ 01_Assignment_variable/27_simplify_exp.py
@@ -0,0 +1,6 @@
+#Simplify expression: x² - 4x + 4 
+x = 3 
+print (f"Original expression: x² - 4x + 4")
+print (f"Value of x: {x}")
+simplified_expression = x**2 - 4*x + 4
+print (f"Simplified expression: {simplified_expression}")
