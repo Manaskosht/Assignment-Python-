7709a26d6c076252f258c835ab17a5ee8f1077f5
Manaskosht <koshtimanas@gmail.com>
2026-05-05 21:02:37 +0530

    string_user

---
+++ 01_Assignment_variable/34_string_user.py
@@ -0,0 +1,8 @@
+Movie_Name = input("Enter your favorite movie name: ")
+Viewer_name = input("Enter your name: ")
+Seat_Number = input("Enter your seat number: ")
+Show_Time = input("Enter the show time: ")
+print(f"Movie Name : {Movie_Name}")
+print(f"Viewer Name : {Viewer_name}")   
+print(f"Seat Number : {Seat_Number}")
+print(f"Show Time : {Show_Time}")
