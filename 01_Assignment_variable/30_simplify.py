582c44de5647fbc193c95909b63f68b57bcc1e6a
Manaskosht <koshtimanas@gmail.com>
2026-05-05 13:53:59 +0530

    simplify

---
+++ 01_Assignment_variable/30_simplify.py
@@ -0,0 +1,8 @@
+#Simplify: (x - y)² 
+x = 10
+y = 6
+print (f"Expression: (x - y)²")
+print (f"Value of x: {x}")
+print (f"Value of y: {y}")
+simplified_expression = (x - y)**2
+print (f"Simplified expression: {simplified_expression}")
