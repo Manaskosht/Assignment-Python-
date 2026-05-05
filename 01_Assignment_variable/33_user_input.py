550d68b61acb7e1823d485a96ef23f72101438bb
Manaskosht <koshtimanas@gmail.com>
2026-05-05 20:57:13 +0530

    string_input

---
+++ 01_Assignment_variable/33_user_input.py
@@ -0,0 +1,10 @@
+Full_Name = input("Enter your full name: ")
+Profession = input("Enter your profession: ")
+Favorite_Quote = input("Enter your favorite quote: ")
+Experience = input("Enter your years of experience: ")
+print(f"Name : {Full_Name}")
+print(f"Profession : {Profession}")
+print(f"Favorite Quote : {Favorite_Quote}")
+print(f"Years of Experience : {Experience} years of experience")
+
+
