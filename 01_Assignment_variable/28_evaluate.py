a7304e0856c22d61871284e0308db40e946f1463
Manaskosht <koshtimanas@gmail.com>
2026-05-05 13:47:38 +0530

    evaluate

---
+++ 01_Assignment_variable/28_evaluate.py
@@ -0,0 +1,8 @@
+#Evaluate: (a + b)(a - b) 
+a = 6
+b = 2
+print (f"Expression: (a + b)(a - b)")
+print (f"Value of a: {a}")
+print (f"Value of b: {b}")
+result = (a + b) * (a - b)
+print (f"Result: {result}")
