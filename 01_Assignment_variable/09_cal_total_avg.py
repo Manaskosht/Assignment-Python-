eea435ae1039ecc92ec38044f4eee27e347211fc
Manaskosht <koshtimanas@gmail.com>
2026-05-06 14:29:00 +0530

    update

--- 01_Assignment_variable/09_cal_total_avg.py
+++ 01_Assignment_variable/09_cal_total_avg.py
@@ -9,6 +9,6 @@ print(f"Subject_2 : {Subject_2}")
 print(f"Subject_3 : {Subject_3}")
 print(f"Subject_4 : {Subject_4}")
 Total_marks = Subject_1 + Subject_2 + Subject_3 + Subject_4
-print(f"Total_marks : {Subject_1 + Subject_2 + Subject_3 + Subject_4}")
+print(f"Total_marks : {Total_marks}")
 Average_marks = Total_marks / 4
 print(f"Average_marks : {Average_marks}")
--- 01_Assignment_variable/32_str_opr.py
+++ 01_Assignment_variable/32_str_opr.py
@@ -10,7 +10,7 @@ print(f"Age : {Age}")
 print(f"City : {City}")
 print(f"Hobby : {Hobby}")
 
-print(f"""Meet {Name},a {Age}-year-old enthusiast from {City}.
+print("""Meet {Name},a {Age}-year-old enthusiast from {City}.
 When not busy with daily tasks,{Name} loves spending time {Hobby}.
 Life in {City} keeps {Name} energetic and curious every single day.
 With coding as a passion, the future looks creative and inspiring for 
