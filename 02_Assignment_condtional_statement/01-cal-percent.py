8da831befaefaf07f8505d57c6a9a48ae1c95c0f
Manaskosht <koshtimanas@gmail.com>
2026-05-07 19:09:45 +0530

    update

--- 02_Assignment_condtional_statement/01-cal-percent.py
+++ 02_Assignment_condtional_statement/01-cal-percent.py
@@ -1,14 +1,14 @@
 #Calculate Profit Percentage
 
 cost_price = int(input("Enter cost price : "))
-selling_price = int(input("Enter selling price :"))
+selling_price = int(input("Enter selling price : "))
 if selling_price > cost_price:
     Profit = selling_price - cost_price
-    print("Profit")
+    print(f"Profit = {Profit}")
     Profit_percent = (Profit / cost_price) * 100
-    print(f"Profit percentage is {Profit_percent}")   
-else:
+    print(f"Profit percentage : {Profit_percent}%")   
+elif cost_price > selling_price:
     Loss = cost_price - selling_price
-    print("Loss")
+    print(f"Loss : {Loss}")
     Loss_percent = (Loss / cost_price) * 100
-    print(f"Loss percentage is {Loss_percent}")
+    print(f"Loss percentage : {Loss_percent}%")
---
+++ 02_Assignment_condtional_statement/04-cal-area-circle.py
@@ -0,0 +1,6 @@
+#Write a program to calculate the area of a circle
+
+Radius_circle = float(input("Enter radius of circle :"))
+pi = 3.14
+Area = pi * (Radius_circle ** 2)
+print(f"Area of circle : {Area}")
